0e413796e4d75bff06758aa30186931408827000
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2016-11-10 16:06:10 -0800

    Further code.

--- code/rEDMtutorial/rEDMtutorial.r
+++ code/rEDMtutorial/rEDMtutorial.r
@@ -20,10 +20,11 @@ pred = c(201, 500)          # the prediction set to test the model
 simplex_output <- simplex(ts, lib, pred)
 ?simplex
 
-plot(simplex_output$E, simplex_output$rho, type = "l",
+plot(simplex_output$E, simplex_output$rho, type = "o",
     xlab = "Embedding Dimension (E)", 
     ylab = "Forecast Skill (rho)")
 # So E=2 is the optimal embedding dimension
+
 
 # ts does not seem to be same as in Sugihara & May 1990:
 plot(ts)
@@ -86,3 +87,41 @@ plot(res4)   # res4 is a time series so it draws lines
 
 res5 = tentSim(n=100, n.skip=0, start=0.123) #, doplot=TRUE)
 plot(res5)   # doesn't crash
+
+# So, now back to trying to reproduce Fig 1a from S&M 90. Except we
+#  don't know the starting point (not that the other results should matter).
+res6 = tentSim(n=1000, n.skip=0, start=0.123)
+plot(res6)
+delta6 = res6[-1] - res6[-length(res6)]
+plot(delta6, type="l", ylim=c(-1, 1))      # some values are definitely more common though
+
+recurrencePlot(res6, m=1, d=1, eps=0.001, end.time=1000) # not clear what that is
+
+# Try alternative initial conditions, can be random
+set.seed(42)
+res7 = tentSim(n=1001, n.skip=0) 
+plot(res7)
+plot(res7[-length(res7)], res7[-1])
+delta7 = res7[-1] - res7[-length(res7)]
+plot(delta7, type="l", ylim=c(-1, 1)) # These don't seem to look like Fig 1a, but maybe the characteristics are indeed the same?
+
+# Continue with vignette, and see if can get rest of Fig 1.
+
+lib = c(1, 500)             # the library to construct the model
+pred = c(501, 1000)         # the prediction set to test the model
+
+simplex_output7 <- simplex(delta7, lib, pred)
+?simplex
+
+plot(simplex_output7$E, simplex_output7$rho, type = "o",
+    xlab = "Embedding Dimension (E)", 
+    ylab = "Forecast Skill (rho)")
+
+simplex_output7
+# This tells us that E=2 is the optimal embedding dimension (only just though).
+
+simplex_output7a <- simplex(delta7, lib, pred, E = 2, tp = 1:10)
+
+plot(simplex_output7a$tp, simplex_output7a$rho, type = "o",
+    xlab = "Time to Prediction (tp)", 
+    ylab = "Forecast Skill (rho)", xlim=c(0,10), ylim=c(0, 1))
